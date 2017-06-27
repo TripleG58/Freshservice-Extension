c1e97d07a8d8777b38ad3e09bb0cb55327f1a85f
DESKTOP-AIJ6NKI\ggehr <ggehrke58@gmail.com>
2017-06-26 21:55:02 -0500

    Fixing rate limit problem
    skipping the http request if it is not needed, but still checking the page's html every second for an update.

--- java.js
+++ java.js
@@ -1,10 +1,26 @@
 $(function()
 {
   setInterval(function(){
+    var doRequest = false;
     var tCountHigh = $('div.offset.ticketlist-total-count > b:nth-child(2)');
     var tCountLow = $('div.offset.ticketlist-total-count > b:first');
     var ticketCount = parseInt($(tCountHigh).text()) 
                     - parseInt($(tCountLow).text()) + 1;
+    for (var i = 0; i < ticketCount && !doRequest; ++i){
+      var elem = $('div.info-data.hideForList > div.emphasize:not(.done):first()');
+      var ticketInfo = elem.text().trim();
+      if (ticketInfo.charAt(0) != 'C'){ 
+        // TODO: optimize by keeping track of which tickets need updating to avoid loop later
+        // either way, this script is not very heavy, and optimization won't really matter.
+        // I also optimized by including "&& !doRequest" in this for loop. That way, italics
+        // breaks out of the loop as soon as we know we need to update the HTML. This does 
+        // not save much time, however, because there are only ever 30 elements max to iterate over.
+        // :( I like optimization...
+        doRequest = true;
+      }
+      elem.addClass('done');
+    }
+    $('div.info-data.hideForList > div.emphasize').removeClass('done');
     $.ajax({
       url: "https://msoe.freshservice.com/helpdesk/tickets.json",
       success: function(result){
