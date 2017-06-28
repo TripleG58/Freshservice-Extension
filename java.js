b60d094f03bc60f27ca9c7af15de98f591f768f0
DESKTOP-AIJ6NKI\ggehr <ggehrke58@gmail.com>
2017-06-27 23:02:19 -0500

    finished & packaged into .crx file to be downloaded, opened, and install

--- java.js
+++ java.js
@@ -1,6 +1,61 @@
 $(function()
 {
+  { // block is for scope management
+    ///* Uncomment this line to eliminate this block of code for testing.
+    // Always do one http request on page load to prevent UI delay.
+    // It's like I'm using setTimeout() as a while loop.
+    // This large block is only here because I cannot use 
+    // setTimeout() like a do-while loop.
+    var tCountHigh = $('div.offset.ticketlist-total-count > b:nth-child(2)');
+    var tCountLow = $('div.offset.ticketlist-total-count > b:first');
+    var ticketCount = parseInt($(tCountHigh).text()) 
+                    - parseInt($(tCountLow).text()) + 1;
+    $.ajax({
+      url: "https://msoe.freshservice.com/helpdesk/tickets.json",
+      success: function(result){
+        console.log("did request");
+        var currentTime = new Date();
+        for (var i = 0; i < ticketCount; ++i){
+          var elem = $('div.info-data.hideForList > div.emphasize:not(.done):first()');
+          var ticketInfo = elem.text().trim();
+          if (ticketInfo.charAt(0) != 'C' || ticketInfo.substring(0,5) == 'Close'){
+            var ticketTime = new Date(result[i].created_at);
+            var tempDiff = 0;
+            var createdStr = "Created: ";
+            // Yeah, this next part was never meant to be read. Have fun debugging! ;D
+            // 1 is a special case in English... :(
+            if ((tempDiff = (currentTime.getFullYear() - ticketTime.getFullYear())) > 1)
+              elem.text(createdStr + "over " + tempDiff + " years ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getFullYear() - ticketTime.getFullYear())) == 1)
+              elem.text(createdStr + "over " + tempDiff + " year ago, " + elem.text());
+            else if ((tempDiff = ((currentTime.getMonth() + 1) - (ticketTime.getMonth() + 1))) > 1)
+              elem.text(createdStr + tempDiff + " months ago, " + elem.text());
+            else if ((tempDiff = ((currentTime.getMonth() + 1) - (ticketTime.getMonth() + 1))) == 1)
+              elem.text(createdStr + tempDiff + " month ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getDate() - ticketTime.getDate())) > 1)
+              elem.text(createdStr + tempDiff + " days ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getDate() - ticketTime.getDate())) == 1)
+              elem.text(createdStr + tempDiff + " day ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getHours() - ticketTime.getHours())) > 1)
+              elem.text(createdStr + "about " + tempDiff + " hours ago, " + elem.text());
+            else if((tempDiff = (currentTime.getHours() - ticketTime.getHours())) == 1)
+              elem.text(createdStr + "about " + tempDiff + " hour ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getMinutes() - ticketTime.getMinutes())) > 1)
+              elem.text(createdStr + tempDiff + " minutes ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getMinutes() - ticketTime.getMinutes())) == 1)
+              elem.text(createdStr + tempDiff + " minute ago, " + elem.text());
+            else if ((tempDiff = (currentTime.getSeconds() - ticketTime.getSeconds())) > 0)
+              elem.text(createdStr + "less than a minute ago, " + elem.text());
+          }
+          elem.addClass('done');
+        }
+        $('div.info-data.hideForList > div.emphasize').removeClass('done');
+      }
+    });
+    //*/
+  }
   setInterval(function(){
+    // Here is where the real magic happens.
     var doRequest = false;
     var tCountHigh = $('div.offset.ticketlist-total-count > b:nth-child(2)');
     var tCountLow = $('div.offset.ticketlist-total-count > b:first');
@@ -9,9 +64,8 @@ $(function()
     for (var i = 0; i < ticketCount; ++i){
       var elem = $('div.info-data.hideForList > div.emphasize:not(.done):first()');
       var ticketInfo = elem.text().trim();
-      if (ticketInfo.charAt(0) != 'C'){ // TODO optimize & add features
+      if (ticketInfo.charAt(0) != 'C' || ticketInfo.substring(0,5) == 'Close')
         doRequest = true;
-      }
       elem.addClass('done');
     }
     $('div.info-data.hideForList > div.emphasize').removeClass('done');
@@ -24,7 +78,7 @@ $(function()
           for (var i = 0; i < ticketCount; ++i){
             var elem = $('div.info-data.hideForList > div.emphasize:not(.done):first()');
             var ticketInfo = elem.text().trim();
-            if (ticketInfo.charAt(0) != 'C'){
+            if (ticketInfo.charAt(0) != 'C' || ticketInfo.substring(0,5) == 'Close'){
               var ticketTime = new Date(result[i].created_at);
               var tempDiff = 0;
               var createdStr = "Created: ";
@@ -59,9 +113,5 @@ $(function()
         }
       });
     }
-  }, 100); // 1 ms was too fast and caused 300+ requests to be sent. 
-  // my guess - kept checking in between each individual element loading?
-  // I would have had to click about 10 times which is possible that I did. hmm.
-  // we'll stick with 100ms. It's fast enough to not be annoying to the user, but 
-  // also avoids sending extra requests to freshservice api.
+  }, 1000);
 });
